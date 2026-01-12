0b4c03d35ec33929189933bbb687a06e24e205ec
wjdqudwls <wjdqudwls100@gmail.com>
2026-01-12 22:33:54 +0900

    댓글 조회 기능 추가

--- next-page/src/main/java/com/team2/nextpage/query/reaction/controller/ReactionQueryController.java
+++ next-page/src/main/java/com/team2/nextpage/query/reaction/controller/ReactionQueryController.java
@@ -1,7 +1,9 @@
 package com.team2.nextpage.query.reaction.controller;
 
+import com.team2.nextpage.common.response.ApiResponse;
 import com.team2.nextpage.query.reaction.dto.response.CommentDto;
 import com.team2.nextpage.query.reaction.service.ReactionQueryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,16 @@ public class ReactionQueryController {
         this.reactionQueryService = reactionQueryService;
     }
 
-    /**
-     * 댓글 목록 조회 API
-     */
-    @GetMapping("/comments/{bookId}")
-    public List<CommentDto> getComments(@PathVariable Long bookId) {
-        return null; // impl
-    }
+  /**
+   * 댓글 목록 조회 API
+   * GET /api/reactions/comments/{bookId}
+   *
+   * @param bookId 조회할 소설의 ID
+   * @return       댓글 목록 (ApiResponse로 래핑)
+   */
+  @GetMapping("/comments/{bookId}")
+  public ResponseEntity<ApiResponse<List<CommentDto>>> getComments(@PathVariable Long bookId) {
+    List<CommentDto> comments = reactionQueryService.getComments(bookId);
+    return ResponseEntity.ok(ApiResponse.success(comments));
+  }
 }
